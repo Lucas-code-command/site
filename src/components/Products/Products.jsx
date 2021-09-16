113086d80fbfabbfc9ca16f6e8f53301126de314
Lucas Moliterno <lucas.moliterno1@gmail.com>
2021-09-16 19:46:03 -0300

    commit

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -9,14 +9,14 @@ const Products = ({ products }) =>{
     const classes = useStyles();
 
     return(
-    <main className ={classes.content}>
+    <main className={classes.content}>
         <div className={classes.toolbar} />
-        <Grid container justifyContent = "center" spacing={4}>
-            {products.map((product) => ( //for each
-                <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
-                    <Product product={product} />
-                </Grid>
-            ))}
+        <Grid container justify="center" spacing={4}>
+          {products.map((product) => (
+            <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
+              <Product product={product} />
+            </Grid>
+          ))}
         </Grid>
     </main>
     )
