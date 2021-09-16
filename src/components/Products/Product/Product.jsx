b2f174a82560f33da0c48d5584f1683a599e7227
Lucas Moliterno <lucas.moliterno1@gmail.com>
2021-09-16 14:44:57 -0300

    commit

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-import {Card, CardMedia, CardContent , CardActions , Typography , IconBotton} from '@material-ui/core';
+import {Card, CardMedia, CardContent , CardActions , Typography , IconButton, IconButton} from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
 
-const Product = () => {
+
+import useStyles from './styles';
+
+const Product = ({ product }) => {
+    const classes = useStyles();
+
+    
     return (
         <Card className= {classes.root}>
             <CardMedia className={classes.media} image='' title={product.name}/>
@@ -12,10 +18,18 @@ const Product = () => {
                         {product.name}
                     </Typography>
                     <Typography variant="h5">
-                        {product.name}
+                        {product.price}
                     </Typography>
                 </div>
+                <Typography variant="h2" color="textSecondary">
+                    {product.description}
+                </Typography>
             </CardContent>
+            <CardActions disableSpacing className={classes.CardActions}>
+                <IconButton aria-label="Add to Card">
+                    <AddShoppingCart />
+                </IconButton>
+            </CardActions>
         </Card>
     )
 }
